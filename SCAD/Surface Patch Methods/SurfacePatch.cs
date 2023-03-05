c8483a1d72ec602261ed844900e205b62b59df6a
tasmektep <alpertasmektep@gmail.com>
2023-03-05 20:42:57 +0300

    Surface Generation is added

--- SCAD/Surface Patch Methods/SurfacePatch.cs
+++ SCAD/Surface Patch Methods/SurfacePatch.cs
@@ -23,10 +23,30 @@ namespace SCAD
         /// <param name="v"> v value </param>
         /// <param name="curves"> boundary curves </param>
         /// <returns></returns>
-        private Point3d Kato_Suv(double u, double v, List<Curve> curves)
+        public Point3d Kato_Suv(double u, double v, List<Curve> curves)
         {
-            new Domain(curves, out List<Curve> domaincurve);
-            Parametrization prm = new Parametrization(Parametrization_Method.Harmonic,domaincurve);
+            var dm = new domain();
+            dm.setSides(curves);
+            dm.Update();
+
+            var mesh = dm.MeshTopology(30);
+            var uvs = dm.parameters(30);
+            var vs = dm.vertices_;
+
+
+            List<Line> lines = new List<Line>();
+            for (int i = 0; i < 6; i++)
+            {
+                lines.Add(new Line(new Point3d(vs[i].X, vs[i].Y, 0), new Point3d(vs[(i + 1) % 6].X, vs[(i + 1) % 6].Y, 0)));
+            }
+            //new Domain(curves, out List<Curve> domaincurve);
+            //new Domain(curves, out List<Curve> domaincurve);
+            //Parametrization prm = new Parametrization(Parametrization_Method.Harmonic,domaincurve);
+            //List<double> si = new List<double>();
+            //List<double> di = new List<double>();
+            //(si, di) = prm.GetPoint(u, v);
+
+            Parametrization prm = new Parametrization(Parametrization_Method.RadialDistanceFunction, lines);
             List<double> si = new List<double>();
             List<double> di = new List<double>();
             (si, di) = prm.GetPoint(u, v);
@@ -38,9 +58,9 @@ namespace SCAD
             for (int i = 0; i < curves.Count; i++)
             {
                 double s = curves[i].Domain.Min + si[i] * (curves[i].Domain.Max - curves[i].Domain.Min);
+                Vector3d crossproduct = Vector3d.CrossProduct(curves[i].TangentAt(s), curves[i].CurvatureAt(s));
 
-                Vector3d T = new Vector3d(); // Ribbon Vector EKSİKKKKK
-                //Vector3d T = frames_[(int)0.02 * resolution_].n;
+                Vector3d T = crossproduct; // Ribbon vector
                 Point3d r = curves[i].PointAt(s) + (di[i] * T);
 
                 r_sum += r * Value[i];
---
+++ SCAD/Test.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace SCAD
+{
+    public class Test : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the Test class.
+        /// </summary>
+        public Test()
+          : base("Test", "Test",
+              "Description",
+             "SCAD", "Transfinite")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("", "", "", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddMeshParameter("", "", "", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<Curve> curves_ = new List<Curve>();
+            DA.GetDataList(0, curves_);
+            int resolution = 30;
+            var dm = new domain();
+            dm.setSides(curves_);
+            dm.Update();
+
+
+
+            var mesh = dm.MeshTopology(resolution);
+            var uvs = dm.parameters(resolution);
+            DA.SetData(0, mesh.Getmesh);
+         
+            var msh = mesh.Getmesh;
+            var pts = new List<Point3d>();
+            var sP = new SurfacePatch();
+            for (int i = 0; i < uvs.Count; i++)
+            {
+                //pts.Add(new Point3d(uvs[i].X, uvs[i].Y, 0));
+                Point3d katoout = sP.Kato_Suv(uvs[i].X, uvs[i].Y, curves_);
+                pts.Add(katoout);
+            }
+
+            msh.Vertices.AddVertices(pts);
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("5587F090-35B5-45DE-8AE8-64D81549853E"); }
+        }
+    }
+}
